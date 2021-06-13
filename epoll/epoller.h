8e7fb8e2cf4cd07326b792be03beaafbf959e985
Mango-Haik <wawaju@qq.com>
2021-06-13 18:14:18 +0800

    -

--- epoll/epoller.h
+++ epoll/epoller.h
@@ -20,10 +20,13 @@ public:
 
     bool DelFd(int fd);
 
+    /*返回事件数目，可设置等待时间默认阻塞等待*/
     int Wait(int timeout = -1);
 
+    /*返回待处理事件链表的第i个fd*/
     int GetEventFd(size_t i) const;
 
+    /*返回第i个fd的事件*/
     uint32_t GetEvents(size_t i) const;
 
     epoll_event GetEpollEvent(size_t i) const;
