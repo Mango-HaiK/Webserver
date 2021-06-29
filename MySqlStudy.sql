88e08e42115c374453fda23924c5ef01b2a30756
Mango-Haik <wawaju@qq.com>
2021-06-29 12:49:38 +0800

    -

--- MySqlStudy.sql
+++ MySqlStudy.sql
@@ -414,3 +414,289 @@ FROM customers, orders, orderitems
 WHERE customers.cust_id = orders.cust_id
   AND orderitems.order_num = orders.order_num
   AND prod_id = 'TNT2';
+
+## 14.创建高级联结
+# 使用表的别名 - 表别名不返回客户机
+SELECT cust_name, cust_contact
+FROM customers AS c, orders AS o, orderitems AS oi
+WHERE c.cust_id = o.cust_id
+  AND oi.order_num = o.order_num
+  AND prod_id = 'TNT2';
+
+# 使用不同的类型联结
+# -自联结
+SELECT p1.prod_id, p1.prod_name
+FROM products AS p1, products AS p2
+WHERE p1.vend_id = p2.vend_id
+  AND p2.prod_id = 'DTNTR';
+
+# -自然联结
+SELECT c.*, o.order_num, o.order_date,
+        oi.prod_id, oi.quantity, oi.item_price
+FROM customers AS c, orders AS o, orderitems AS oi
+WHERE c.cust_id = o.cust_id
+  AND oi.order_num = o.order_num
+  AND prod_id = 'FB';
+
+## 15.组合查询 - 通过UNION组合两个查询(也可使用WHERE组合)
+SELECT vend_id, prod_id, prod_price
+FROM products
+WHERE prod_price <= 5
+UNION
+SELECT vend_id, prod_id, prod_price
+FROM products
+WHERE vend_id IN (1001, 1002);
+
+# 使用UNION ALL可返回所有匹配的行，没有ALL将去重
+# 也可对数据进行ORDER BY进行排序，但只能有一个
+SELECT vend_id, prod_id, prod_price
+FROM products
+WHERE prod_price <= 5
+UNION ALL 
+SELECT vend_id, prod_id, prod_price
+FROM products
+WHERE vend_id IN (1001, 1002)
+ORDER BY vend_id, prod_price;
+
+
+## 16.全文本搜索
+# **仅在MyISAM支持全文本搜索
+# Match指定查找的列，Against指定搜索表达式,BIHARY区分大小写 
+SELECT note_text
+FROM productnotes
+WHERE Match(note_text) Against('rabbit');
+
+# 查询扩展，搜索表达式后加WITH QUERY EXPANSION
+#两次查询，第一次找到所在的行，然后在该行检查有用的词(存在的词)
+# 之后使用这些词进行第二次查询，返回匹配的行
+SELECT note_text
+FROM productnotes
+WHERE Match(note_text) Against('anvils' WITH QUERY EXPANSION);
+
+# 布尔文本搜索，搜索表达式后加 IN BOOLENAN MODE
+# 没有FULLTEXT索引也可以使用
+# 匹配heavy,但是如果出现rope则排除
+SELECT note_text
+FROM productnotes
+WHERE Match(note_text) Against('heavy -rope*' IN BOOLEAN MODE);
+
+/*
+|布尔操作符|说明|
+|----|----|
+|+|包含，词必须存在|
+|-|排除，词必须不出现|
+|>|包含，而且增加等级值|
+|<|包含，而且减少等级值|
+|()|把词组成字表达式(允许这些子表达式作为一个组被包含、排除和排序等)|
+|~|取消一个词的排序值|
+|*|词尾的通配符|
+|""|定义一个短语（与单个词的列表不一样，他匹配短语以便包含或排除这个短语）|
+*/
+
+SELECT note_text
+FROM productnotes
+WHERE Match(note_text) Against('+rabbit +bait' IN BOOLEAN MODE);
+
+# 至少出现rabbit和bait的一个
+SELECT note_text
+FROM productnotes
+WHERE Match(note_text) Against('rabbit bait' IN BOOLEAN MODE);
+
+# 匹配短语""，而不是单个词
+SELECT note_text
+FROM productnotes
+WHERE Match(note_text) Against('"rabbit bait"' IN BOOLEAN MODE);
+
+# 匹配safe和combination，降低后者等级
+SELECT note_text
+FROM productnotes
+WHERE Match(note_text) Against('+safe + +(<combination)' IN BOOLEAN MODE);
+
+## 17.插入数据
+# 插入完整的行
+INSERT INTO customers
+VALUES (NULL,
+        'Pep E. LaPew',
+        '100 Main Street',
+        'Los Angeles',
+        'CA',
+        '90046',
+        'USA',
+        NULL,
+        NULL);
+
+# 也可以这样，更安全，但也更繁琐,列名可不按表的顺序，但是值必须得对应
+# 也可以省略列，但需要满足列可为NULL，或提供了默认值
+INSERT INTO customers(cust_name,
+        cust_address,
+        cust_city,
+        cust_state,
+        cust_zip,
+        cust_country,
+        cust_contact,
+        cust_email)
+VALUES (NULL,
+        'Pep E. LaPew',
+        '100 Main Street',
+        'Los Angeles',
+        'CA',
+        '90046',
+        'USA',
+        NULL,
+        NULL);
+
+# 插入多个行
+INSERT INTO customers(cust_name,
+      cust_address,
+      cust_city,
+      cust_state,
+      cust_zip,
+      cust_country
+)
+VALUES(
+      'Pep E. LaPew',
+      '100 Main Street',
+      'Los Angeles',
+      'CA',
+      '90046',
+      'USA'
+),(
+      'M. Martian',
+      '42 Galaxy Way',
+      'New York',
+      'NY',
+      '11213',
+      'USA'
+);
+
+# *使用单个INSERT语句插入多个行速度会快些
+
+# 插入检索出的数据
+INSERT INTO customers(cust_name,
+        cust_address,
+        cust_city,
+        cust_state,
+        cust_zip,
+        cust_country,
+        cust_contact,
+        cust_email)
+VALUES(SELECT cust_name,
+        cust_address,
+        cust_city,
+        cust_state,
+        cust_zip,
+        cust_country,
+        cust_contact,
+        cust_email
+      FROM custnew);
+
+# *INSERT语句也可以包含WHERE子句过滤插入的数据
+
+## 18.更新和删除数据
+# *不要省略WHERE子句，不然会更新整个表
+UPDATE customers
+SET cust_email = 'elmer@fudd.com'
+WHERE cust_id = 10005;
+
+# 更新多个列
+UPDATE customers
+SET cust_name = 'The Fudds',
+    cust_email = 'elemer@fudd.com'
+WHERE cust_id = 10005;
+
+# *UPDATE语句也可使用子查询
+# *IGNORE关键字，更新多行时指定可在出错时继续执行剩下的语句
+# *用法：UPDATE IGNORE customers...
+
+# 删除数据
+# *务必记得使用WHERE不然会删除整个表的行
+DELETE FROM customers
+WHERE cust_id = 10006;
+
+# *DELETE删除整行而不是列，删除列可使用UPDATE
+# *TRUNCATE TABLE 语句删除原来的表再重新创建一个表(效率更快)
+# *在使用UPDATE和DELETE时先使用SELETE进行测试，以保证过滤的是否正确
+
+## 19.创建表和操纵表
+# 表创建基础 - customers表的创建语句
+CREATE TABLE customers /*IF NOT EXISTS （表名不存在时创建）*/
+(
+        cust_id      INT        NOT NULL AUTO_INCREMENT ,
+        cust_name    char(50)   NOT NULL ,
+        cust_address char(50)   NULL ,
+        cust_city    char(50)   NULL ,
+        cust_state   char(5)    NULL ,
+        cust_zip     char(10)   NULL ,
+        cust_country char(50)   NULL DEFAULT 'China',
+        cust_contact char(50)   NULL ,
+        cust_email   char(255)  NULL ,
+        PRIMARY(cust_id)        /*设定主键*/
+) ENGINE = InnoDB;
+
+# *NULL指定可为空值，加上NOT则不可为空值
+# *PRIMARY(order_id, order_num),多个主键可这样写
+# *AUTO_INCREMENT,自动增量，每个表只允许一个
+# *DEFAULT，指定默认值
+
+# 引擎类型
+/*
+|引擎|说明|
+|----|----|
+|InnoDB|可靠的事务处理引擎，但不支持全文本搜索|
+|MyISAM|性能极高的引擎，支持全文本搜索但不支持事务处理|
+|MEMORY|等同于MyISAM，但由于数据存储在内存中，速度很快（适合与临时表）|
+*/
+# * InnoDB为MySQL的默认引擎
+# * 引擎类型可以混用，但是外键不能跨引擎
+
+# 更新表
+#* 可使用ALTER TABLE修改表，但是在存储数据后就不应再被更新。
+#* 给一个表增加一个名为vend_phone的列，必须明确其数据类型。
+;
+ALTER TABLE vendors
+ADD vend_phone CHAR(20);
+
+# *删除一个列
+ALTER TABLE vendors
+DROP COLUMN vend_phone;
+
+# *定义外键
+/*
+ALTER TABLE <数据表名> ADD CONSTRAINT <外键名>
+FOREIGN KEY (<列名>) REFERENCES <主表名>(<列名>)
+*/
+ALTER TABLE orderitems
+ADD CONSTRAINT fk_orderitems_orders
+FOREIGN KEY (order_num) REFERENCES orders (order_num);
+
+# 删除表
+# *如果存在则删除，没有确认，也不能撤销，永久删除
+DROP TABLE customers2;
+
+# 重命名表
+# *可在后面加上  ,<表名> TO <新表名>进行多个表的重命名
+RENAME TABLE customers2 TO customers;
+
+## 20.视图
+# *视图是对表的查询结果，索引并不存储数据
+# *使用多个联结和过滤创建了复杂的视图会降低性能
+# *和表一样视图必须唯一命名，创建的视图的数目没有限制
+# *创建视图需要足够的访问权限
+# *视图也可与嵌套
+# *ORDER BY也可用在视图中，但如果该视图检索数据的SELECT语句中也包含有ORDER BY，那么该视图中的ORDER BY将会被覆盖
+# *视图不能索引，也不能关联的触发器或默认值
+# *视图可与表一起使用，如SELECT。
+
+# 使用视图
+# *视图使用CREATE VIEW 创建
+# *SHOW CHREAT VIEW 查看创建视图的语句
+# *DROP VIEW <viewname>,删除视图
+# *更新视图可使用DROP再使用CREATE，也可直接使用CREAT REPLACE VIEW
+
+# 利用视图简化复杂的联结
+;
+CREATE VIEW porductcustomers AS 
+SELECT cust_name, cust_contact, prod_id
+FROM customers, orders, orderitems
+WHERE customers.cust_id = orders.cust_id
+  AND orderitems.order_num = orders.order_num;
